c659484e14ff8d45d8dc889f9dc2d8f16f03cfea
Luyba <lsorlova82@gmail.com>
2023-01-17 07:29:11 +0200

    from home

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,2 +1,20 @@
 public class Manager {
+    private String[] films = new String[0];
+    public void addFilm(String film) {
+        String[] tmp = new String[films.length +1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast() {
+
+    }
+
 }
